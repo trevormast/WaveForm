1b798eb67a187318c98bf34c32485bdd537ae0f6
trevormast <trevormast@gmail.com>
2016-07-31 20:34:37 -0700

    Adds toggle button for resultant wave
    Cleans up JS

--- public/assets/js/canvas.js
+++ public/assets/js/canvas.js
@@ -1,9 +1,13 @@
 var canvas = $('#myCanvas');
 
 function drawWave(points, waveType) {
-
-  var obj = waveTypeCheck(waveType);
-
+  var options = {
+                  layer: true,
+                  name: waveType,
+                  opacity: 0.75,
+                  rounded: true
+                };
+  var obj = waveTypeCheck(waveType, options);
   var pts = points;
 
   for (var p = 0; p < pts.length; p += 1) {
@@ -14,27 +18,41 @@ function drawWave(points, waveType) {
   $('#myCanvas').drawLine(obj);
 };
 
-var waveTypeCheck = function(waveType) {
-  if ( waveType === 'resultant' ) {
-    var options = {
-                    layer: true,
-                    name: waveType,
-                    opacity: 0.75,
-                    strokeStyle: '#F00',
-                    strokeWidth: 6,
-                    rounded: true
-                  }
-  } else {
-    var options = {
-                    layer: true,
-                    name: waveType,
-                    opacity: 0.75,
-                    strokeStyle: '#000',
-                    strokeWidth: 3,
-                    rounded: true,
-                    visible: false
+var waveTypeCheck = function(waveType, options) {
+  var resultantOptions  = { strokeStyle: '#F00',
+                            strokeWidth: 6
+                          };
 
-                  }
-  };
+  var originalOptions   = { strokeStyle: '#000',
+                            strokeWidth: 3,
+                            visible: false
+                          };
+
+  var additionalOptions = ( waveType === 'resultant'
+                            ? resultantOptions
+                            : originalOptions
+                          );
+
+
+  pushOptions(options, additionalOptions);
   return options
+};
+
+function pushOptions(options, additionalOptions) {
+  for (var key in additionalOptions) {
+    options[key] = additionalOptions[key];
+  }
+}
+
+function toggleVisibility(layer) {
+  var toggler = ($('canvas').getLayer(layer).visible == false) ? true : false
+  $('canvas').setLayer(layer, {
+      visible: toggler
+    }).drawLayers();
+};
+
+function resetCanvas() {
+  $('canvas').removeLayer('resultant').drawLayers();
+  $('canvas').removeLayer('freq1').drawLayers();
+  $('canvas').removeLayer('freq2').drawLayers();
 };
